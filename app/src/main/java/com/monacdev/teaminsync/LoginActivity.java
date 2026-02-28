289e4afdbc7d8e4db36579e57dc56f947388444c
andre <andreamonacelli02@gmail.com>
2026-02-28 18:05:58 +0100

    Initial Development: - Connected the project to Firebase - Defined the Login Activity (still need to develop the new user creation wizard to populate the DB) - Defined the interface for both LoginActivity and MainActivity

---
+++ app/src/main/java/com/monacdev/teaminsync/LoginActivity.java
@@ -0,0 +1,152 @@
+package com.monacdev.teaminsync;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText emailInputET;
+    private EditText passwordET;
+    private Button signInBtn;
+    private Button registerBtn;
+    private final FirebaseAuth authClient = FirebaseAuth.getInstance();
+    private final DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        this.bindViewsToObjects();
+        /* Binding listeners to buttons */
+        this.signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userSignIn();
+            }
+        });
+        this.registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+            }
+        });
+    }
+
+    /**
+     * Binds the Views defined within the XML layout file for the activity to their respective Java objects
+     */
+    private void bindViewsToObjects(){
+        this.emailInputET = findViewById(R.id.emailInputET);
+        this.passwordET = findViewById(R.id.passwordET);
+        this.signInBtn = findViewById(R.id.signInBtn);
+        this.registerBtn = findViewById(R.id.registerBtn);
+    }
+
+    /**
+     * Manages Sign-In procedure leveraging Firebase API
+     */
+    private void userSignIn(){
+        String email = this.emailInputET.getText().toString();
+        String password = this.passwordET.getText().toString();
+        this.authClient.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser loggedUser = authClient.getCurrentUser();
+                    Toast.makeText(LoginActivity.this, R.string.auth_ok, Toast.LENGTH_SHORT).show();
+                    if(loggedUser != null){
+                        navigateToNextActivity(loggedUser);
+                    } else {
+                        Toast.makeText(LoginActivity.this, R.string.auth_session_creation_err, Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(LoginActivity.this, R.string.auth_generic_err, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    /**
+     * Manages User Creation procedure leveraging Firebase API
+     */
+    private void registerUser(){
+        String email = this.emailInputET.getText().toString();
+        String password = this.passwordET.getText().toString();
+        this.authClient.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser loggedUser = authClient.getCurrentUser();
+                    Toast.makeText(LoginActivity.this, R.string.register_ok, Toast.LENGTH_SHORT).show();
+                    /* TODO: Populating the DB with the newly created user through a wizard */
+                    if(loggedUser != null){
+                        navigateToNextActivity(loggedUser);
+                    } else {
+                        Toast.makeText(LoginActivity.this, R.string.auth_session_creation_err, Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(LoginActivity.this, R.string.register_err, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    /**
+     * Manages user data retrieval from the Firebase DB and switches to either the HomePage or the WaitingForApprovalPage
+     * @param loggedUser instance of the user that just logged in
+     */
+    private void navigateToNextActivity(FirebaseUser loggedUser){
+        String userEmail = loggedUser.getEmail();
+        Query q = this.dbRef.getDatabase().getReference("users").orderByChild("email").equalTo(userEmail);
+        q.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    for(DataSnapshot userSnapshot : snapshot.getChildren()){
+                        String loggedUserID = userSnapshot.getKey();
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        intent.putExtra("logged_user_id", loggedUserID);
+                        startActivity(intent);
+                        finish(); /* Terminates Login Activity */
+                    }
+                } else {
+                    Toast.makeText(LoginActivity.this, "ATTENZIONE: Utente con mail " + userEmail + " non trovato nel sistema!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(LoginActivity.this, R.string.connection_err, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
--- app/src/main/java/com/monacdev/teaminsync/MainActivity.java
+++ app/src/main/java/com/monacdev/teaminsync/MainActivity.java
@@ -1,6 +1,12 @@
 package com.monacdev.teaminsync;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView teamLogoIV;
+    private TextView homepageHeaderTV;
+    private Button toTrainingPageBtn;
+    private Button squadListBtn;
+    private TextView teamNameTV;
+    private TextView leagueNameTV;
+    private TextView cityStadiumTV;
+    private ImageButton openNotificationsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,45 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        this.bindViewsWithObjects();
+
+        /* Defining Button Listeners */
+        this.squadListBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent squadListIntent = new Intent(MainActivity.this, MembersListActivity.class);
+                startActivity(squadListIntent);
+            }
+        });
+        this.toTrainingPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent trainingPageIntent = new Intent(MainActivity.this, TrainingActivity.class);
+                startActivity(trainingPageIntent);
+            }
+        });
+        this.openNotificationsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                * TODO: implement a Fragment to show notifications
+                */
+            }
+        });
+    }
+
+    /**
+     * Binds the Views defined within the XML layout file for the activity to their respective Java objects
+     */
+    private void bindViewsWithObjects(){
+        this.teamLogoIV = findViewById(R.id.teamLogoIV);
+        this.homepageHeaderTV = findViewById(R.id.homepageHeaderTV);
+        this.toTrainingPageBtn = findViewById(R.id.toTrainingPageBtn);
+        this.squadListBtn = findViewById(R.id.squadListBtn);
+        this.teamNameTV = findViewById(R.id.teamNameTV);
+        this.leagueNameTV = findViewById(R.id.leagueNameTV);
+        this.cityStadiumTV = findViewById(R.id.cityStadiumTV);
+        this.openNotificationsBtn = findViewById(R.id.openNotificationsBtn);
     }
 }
