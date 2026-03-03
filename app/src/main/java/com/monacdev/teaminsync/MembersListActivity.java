a914dd59664c447a4d153f2f57f7a39b62e203b1
andreamonacelli <andreamonacelli02@gmail.com>
2026-03-03 17:26:12 +0100

    Initial Development - partial commit

--- app/src/main/java/com/monacdev/teaminsync/MembersListActivity.java
+++ app/src/main/java/com/monacdev/teaminsync/MembersListActivity.java
@@ -7,8 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MembersListActivity extends AppCompatActivity {
+    private final DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+    private RecyclerView coachesListRV;
+    private RecyclerView athletesListRV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,22 @@ public class MembersListActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        this.bindViewsToObjects();
+    }
+
+    /**
+     * Given the respective XML file, this method binds each view to its corresponding Java object
+     */
+    private void bindViewsToObjects(){
+        this.coachesListRV = findViewById(R.id.coachesListRV);
+        this.athletesListRV = findViewById(R.id.athletesListRV);
+    }
+
+    /**
+     * Fetches the list of users from the Firebase DB in order to populate the lists
+     */
+    private void fetchDataFromDB(){
+
     }
 }
